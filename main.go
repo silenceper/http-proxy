19c63297f3e09cd044d96a19ae99b306184fcf9e
wenzl <wenzl@touzhijia.com>
2018-07-06 18:25:30 +0800

    add listen log

--- main.go
+++ main.go
@@ -12,5 +12,6 @@ func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = true
+	log.Println("Listen :8080")
 	log.Fatal(http.ListenAndServe(":8080", proxy))
 }
